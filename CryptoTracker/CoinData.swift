f5bef53ad3c347f6d379414f329266b166bce5f4
Rommel Rico <me@rommelrico.com>
2018-05-16 08:47:59 -0700

    CoinData model

---
+++ CryptoTracker/CoinData.swift
@@ -0,0 +1,17 @@
+//
+//  CoinData.swift
+//  CryptoTracker
+//
+//  Created by Rommel Rico on 5/16/18.
+//  Copyright © 2018 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class Coin {
+    var symbol = ""
+    var image = UIImage()
+    var price = 0.0
+    var amount = 0.0
+    var historicalData = [Double]()
+}
