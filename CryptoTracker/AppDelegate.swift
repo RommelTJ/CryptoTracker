f9c1b5b28ecb9b057a60131eeacb5003efb10e6b
Rommel Rico <me@rommelrico.com>
2018-05-16 08:40:22 -0700

    Making the CryptoTableViewController as root View Controller

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Making the CryptoTableViewController as root view controller.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let cryptoTableVC = CryptoTableViewController()
+        window?.rootViewController = cryptoTableVC
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
