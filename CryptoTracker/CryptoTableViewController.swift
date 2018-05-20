ee71d6e7a02d6674313c2fd7769365eba07c0a52
Rommel Rico <me@rommelrico.com>
2018-05-19 20:40:45 -0700

    Adding the prices to the CryptoTableViewController cells

--- CryptoTracker/CryptoTableViewController.swift
+++ CryptoTracker/CryptoTableViewController.swift
@@ -24,7 +24,7 @@ class CryptoTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         let coin = CoinData.shared.coins[indexPath.row]
-        cell.textLabel?.text = coin.symbol
+        cell.textLabel?.text = "\(coin.symbol) - \(coin.price)"
         cell.imageView?.image = coin.image
         return cell
     }
