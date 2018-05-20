89db866a19156520b234317bd358d7163dee0e40
Rommel Rico <me@rommelrico.com>
2018-05-20 16:22:14 -0700

    Calling CryptoCompare to get historical price data

--- CryptoTracker/CoinViewController.swift
+++ CryptoTracker/CoinViewController.swift
@@ -11,19 +11,31 @@ import SwiftChart
 
 private let chartHeight: CGFloat = 300.0
 
-class CoinViewController: UIViewController {
+class CoinViewController: UIViewController, CoinDataDelegate {
 
     var coin: Coin?
     var chart = Chart()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        CoinData.shared.delegate = self
+        coin?.getHistoricalData()
+        
         view.backgroundColor = .white
         chart.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: chartHeight)
-        let series = ChartSeries([0, 6, 2, 8, 4, 7, 3, 10, 8])
-        series.color = ChartColors.greenColor()
-        chart.add(series)
         view.addSubview(chart)
+    }
+    
+    // MARK: - CoinDataDelegate
+    
+    func newHistory() {
+        if let coin = coin {
+            let series = ChartSeries(coin.historicalData)
+            series.area = true
+            chart.add(series)
+        }
+        
     }
 
 }
