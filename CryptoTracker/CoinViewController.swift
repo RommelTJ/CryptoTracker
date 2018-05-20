8a6035c7a2d29e2ef36cb2a5d38dbefd7407a269
Rommel Rico <me@rommelrico.com>
2018-05-20 16:00:22 -0700

    Setting the background color to white

--- CryptoTracker/CoinViewController.swift
+++ CryptoTracker/CoinViewController.swift
@@ -14,8 +14,7 @@ class CoinViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
     override func didReceiveMemoryWarning() {
